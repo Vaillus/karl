76c98e3a732d6d4780a3138abfe84688d7676751
Kathryn Schutte <kathryn.schutte@gmail.com>
2018-03-19 17:05:24 +0100

    Meet the front end !!

---
+++ MyAppKarl/components/Ootd.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Text, View } from 'react-native';
+
+
+export default class Ootd extends React.Component{
+
+	static navigationOptions= {
+		title: 'ootd',
+
+	};
+
+	render (){
+		return (
+			<View>
+	       		<Text> This is a test</Text>
+	      	</View>
+	    );
+	}
+
+}
