74e448578128e77a35b9f4209fac98192007c89a
hugovaillaud <hugovaillaud@gmail.com>
2018-04-09 13:46:05 +0200

    blindage des paramètres de getOutfit et prise en compte des unwanted clothes

--- testConnection.py
+++ testConnection.py
@@ -14,17 +14,10 @@ with open('clothes.json') as json_data:
  #   users = json.load(json_data)
 #users = open('users.json')
 #print(users[0]);
-#outfit = outfit.getOutfit(clothes)
-#socks = outfit.getClothesbyCat(d, 'socks')
 
-#for i in range(0, len(outfit)):
-#    print(outfit[i])
 newClothes = list()
 for i in range(1, len(clothes)-1):
 	newClothes.append(clothes[i])
-#for i in range(0, len(clothes)-1):
-#	if (clothes[i]['category'] == "jacket" or clothes[i]['category'] == "pants" or clothes[i]['category'] == "tshirt" or clothes[i]['category'] == "hoodie" or clothes[i]['category'] == "long sleeves tshirt" or clothes[i]['category'] == "sweater" or clothes[i]['category'] == "short"):
-#		print(newClothes[i]['name'])
 
 print(newClothes[0])
 myOutfit = outfit.getOutfit(newClothes)
