6ed1c92ff7ea7bb8ac01400fb3ea1c302bbb2abb
hugovaillaud <hugovaillaud@gmail.com>
2018-04-10 17:00:41 +0200

    amélioration de la sécurité de la connexion et ajout de la fonction de connection

--- ML/testConnection.py
+++ ML/testConnection.py
@@ -1,25 +1,40 @@
 #import json
 import outfit
-
 import pymongo
+import re
 
 #To make the following line work, make sure that mongod is running on your computer.
 #To do so, install mongoDB and run the command: 'sudo service mongod start'
 
-client = pymongo.MongoClient('mongodb://BenoitLeguay:karlos2018@ds139459.mlab.com:39459/karl')
+file = open("donnéesSecrètes.txt")
+secretData = re.sub('\\n', '', file.read())
+
+client = pymongo.MongoClient(secretData)
 db = client.karl
 collection = db.clothes
-print(collection.find()[0])
+
 #with open('clothes.json') as json_data:
 #    clothes = json.load(json_data)
-
-
+clothes = list(collection.find({}))
 newClothes = list()
-for i in range(1, len(collection)-1):
-	newClothes.append(collection[i])
+for i in range(1, len(clothes)-1):
+	newClothes.append(clothes[i])
 
 print(newClothes[0])
 myOutfit = outfit.getOutfit(newClothes)
 print(myOutfit)
 myOutfit = outfit.getOutfit(db=newClothes, outfit=myOutfit, bpNeeded=[1])
 print(myOutfit)
+
+#returns the dataBase. Not to be used on the long term
+def getDataBase():
+	file = open("donnéesSecrètes.txt")
+	secretData = re.sub('\\n', '', file.read())
+
+	client = pymongo.MongoClient(secretData)
+	db = client.karl
+	collection = db.clothes
+
+	clothes = list(collection.find({}))
+
+	return clothes
