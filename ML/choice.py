635f6f94edb5061066c305d5f0227a7d3c8def04
hugovaillaud <hugovaillaud@gmail.com>
2018-04-11 12:38:00 +0200

    je veux juste pull poto

--- ML/choice.py
+++ ML/choice.py
@@ -6,34 +6,34 @@ Created on Mon Apr  9 14:42:10 2018
 @author: hugo
 """
 import json
-import pymongo
 import outfit
+import testConnection
 from bson import ObjectId
+
 
 class JSONEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, ObjectId):
             return str(o)
         return json.JSONEncoder.default(self, o)
-    
+
+
 def makeDecision(myoutfit, decision):
     decision = True
-    client = pymongo.MongoClient('mongodb://BenoitLeguay:karlos2018@ds139459.mlab.com:39459/karl')
-    db = client.karl
-    collection = db.clothes
-    cursor = list(collection.find({}))
+
+    cursor = testConnection.getDataBase()
+
     myoutfit = outfit.getOutfit(cursor)
     data = {}
     idc = 0
     for clothe in myoutfit:
         data['idc'+str(idc)] = clothe['_id']
         idc = idc + 1
-        
+
     data['decision'] = decision
-    db.decision.insert(data)
     print(data)
     json_data = JSONEncoder().encode(data)
     print(json_data)
-    
-    
-makeDecision()
+
+
+#makeDecision()
--- ML/testConnection.py
+++ ML/testConnection.py
@@ -6,7 +6,7 @@ import re
 #To make the following line work, make sure that mongod is running on your computer.
 #To do so, install mongoDB and run the command: 'sudo service mongod start'
 
-
+#returns the dataBase. Not to be used on the long term
 def getDataBase():
 	file = open("donnéesSecrètes.txt")
 	secretData = re.sub('\\n', '', file.read())
@@ -21,7 +21,9 @@ def getDataBase():
 
 #with open('clothes.json') as json_data:
 #    clothes = json.load(json_data)
+
 clothes = getDataBase()
+
 newClothes = list()
 for i in range(1, len(clothes)-1):
 	newClothes.append(clothes[i])
@@ -31,5 +33,3 @@ myOutfit = outfit.getOutfit(newClothes)
 print(myOutfit)
 myOutfit = outfit.getOutfit(db=newClothes, outfit=myOutfit, bpNeeded=[1])
 print(myOutfit)
-
-#returns the dataBase. Not to be used on the long term
