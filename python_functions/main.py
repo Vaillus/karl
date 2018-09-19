29b14b5f3fc4256cda679d9f59b41e762d816419
hugovaillaud <hugovaillaud@gmail.com>
2018-09-19 16:01:51 +0200

    changements de nom de sfichiers

--- python_functions/main.py
+++ python_functions/main.py
@@ -1,6 +1,6 @@
 import sys
-import var_static
-import create_outfits.Outfit.outfit as outfit
+import variables
+import python_functions.Outfit.outfit as outfit
 sys.path.remove('/home/benoit/Sensego/Missions/user_profile')
 sys.path.append(var_static.DATABASE)
 print(sys.path)
