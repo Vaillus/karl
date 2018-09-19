d26e52f48050cfb8c689247cdfe00b523629b504
Benoit <benoit.leguay@gmail.com>
2018-09-19 14:59:19 +0200

    a wild gitignore appears also in dev git add .gitignore

--- python_functions/main.py
+++ python_functions/main.py
@@ -1,5 +1,11 @@
-
-
-
+import sys
+import var_static
+import create_outfits.Outfit.outfit as outfit
+sys.path.remove('/home/benoit/Sensego/Missions/user_profile')
+sys.path.append(var_static.DATABASE)
+print(sys.path)
 if __name__ == '__main__':
-    pass
+    a = 2
+    file = open("clothes.json")
+    outfit.get_outfit()
+
