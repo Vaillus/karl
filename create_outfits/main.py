237509cfc67784890332f64eed46dc63eb5227b8
Benoit <benoit.leguay@gmail.com>
2018-09-19 14:44:40 +0200

    path management

--- create_outfits/main.py
+++ create_outfits/main.py
@@ -1,8 +1,11 @@
-import sys, os
-
+import sys
+import var_static
 import create_outfits.Outfit.outfit as outfit
-
-
+sys.path.remove('/home/benoit/Sensego/Missions/user_profile')
+sys.path.append(var_static.DATABASE)
+print(sys.path)
 if __name__ == '__main__':
+    a = 2
+    file = open("clothes.json")
     outfit.get_outfit()
 
