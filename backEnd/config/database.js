403ca591bcc1a3e38691c119d2c76f4c47a6d628
hugovaillaud <hugovaillaud@gmail.com>
2018-09-25 15:30:54 +0200

    removed uppercase character at the beggining of backEnd folder

---
+++ backEnd/config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+    password: 'admin',
+    databaseAddress: 'ds139459.mlab.com',
+    username: 'Admin',
+    port: '39459',
+};
---
+++ backEnd/models/first.js
@@ -0,0 +1,23 @@
+var client = require('../helpers/mongoClient');
+const util = require('util')
+
+exports.create = function(values) {
+
+};
+
+exports.get = function(values) {
+
+};
+
+exports.getById = function(query) {
+
+};
+
+
+exports.update = function(body) {
+
+};
+
+exports.delete = function(id) {
+
+};
